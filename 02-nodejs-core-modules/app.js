79c13f0810b8e4744345bc6be66974fdbb8d9309
MarwanOladele <oladelemarwan@gmail.com>
2023-03-26 12:40:33 +0100

    new commit

--- 02-nodejs-core-modules/app.js
+++ 02-nodejs-core-modules/app.js
@@ -1,8 +1,28 @@
 const fs = require("fs");
 
-const fileContent = fs.readFileSync("./messages.txt", "utf-8");
-console.log(fileContent);
+// Synchronous way
+// const fileContent = fs.readFileSync("./messages.txt", "utf-8");
+// console.log(fileContent);
 
-const newFileContent = `This is a new file content: ${fileContent}`;
-fs.writeFileSync("data.txt", newFileContent, "utf-8");
-console.log('Data is written to a file');
+// const newFileContent = `This is a new file content: ${fileContent}`;
+// fs.writeFileSync("data.txt", newFileContent, "utf-8");
+// console.log("Data is written to a file");
+
+// Asynchronous way
+// fs.readFile("./messages.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(data);
+// });
+
+fs.readFile("./messages.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+
+  fs.writeFile("hello.txt", data, "utf-8", (err) => {
+    if (err) throw err;
+    console.log("data is returned");
+  }); 
+});
+
+console.log('This is a FS core module example');

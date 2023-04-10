7cb83eb5bb1ec2a0c25f2857cb8172af2becf122
MarwanOladele <oladelemarwan@gmail.com>
2023-04-10 12:57:16 +0100

    new commit

--- 10-nodejs-create-server/server.js
+++ 10-nodejs-create-server/server.js
@@ -1,6 +1,5 @@
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const router = require("./router/router");
 
 const hostname = "localhost";
 const port = 5900;
@@ -8,29 +7,7 @@ const port = 5900;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
 
-  const url = req.url;
-
-  if (url === "/") {
-    fs.readFile(path.join(__dirname, "index.html"), "utf8", (err, data) => {
-      if (err) throw err;
-      res.end(data);
-    });
-  } else if (url === "/about") {
-    fs.readFile(path.join(__dirname, "about.html"), "utf8", (err, data) => {
-      if (err) throw err;
-      res.end(data);
-    });
-  } else if (url === "/contact") {
-    fs.readFile(path.join(__dirname, "contact.html"), "utf8", (err, data) => {
-      if (err) throw err;
-      res.end(data);
-    });
-  } else {
-    fs.readFile(path.join(__dirname, "404.html"), "utf8", (err, data) => {
-      if (err) throw err;
-      res.end(data);
-    });
-  }
+  router.routeFun(req, res);
 });
 
 server.listen(port, hostname, () => {

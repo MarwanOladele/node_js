c41a99ff6cb901b5df9143fb53fd0e9be5effb0f
MarwanOladele <oladelemarwan@gmail.com>
2023-04-30 22:19:59 +0100

    expressjs basics

--- 11-express-js/app.js
+++ 11-express-js/app.js
@@ -1,17 +1,10 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-app.use(express.static('./public'))
-
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "navbar-app", "index.html"));
-});
-
-app.all("*", (req, res) => {
-  res.status(404).send("Rsources not found");
+app.get('/', (req, res) => {
+  res.json([{ name: "Marwan" }, { name: "Temitope" }]);
 });
 
 app.listen(5000, () => {
-  console.log("Server listening on port 5000...");
+  console.log('Server listening on port 5000...');
 });

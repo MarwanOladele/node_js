d2c9a474c77169b9c0db892b01874201bac2f7bb
MarwanOladele <oladelemarwan@gmail.com>
2023-04-30 21:37:12 +0100

    my first expressjs code

--- 11-express-js/app.js
+++ 11-express-js/app.js
@@ -1,20 +1,17 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-app.get("/", (req, res) => {
-  console.log("user hit the resource");
-  res.status(200).send("Home page");
-});
+app.get('/', (req, res) => {
+    res.status(200).send('yaaaaaaay')
+})
 
-app.get("/about", (req, res) => {
-  console.log("user hit the resource");
-  res.status(200).send("About page");
-});
 
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>resource not found!</h1>");
-});
+app.all('*', (req, res) => {
+    res.status(400).send('yaaaaaaay')
+})
+
+
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000...");
-});
+    console.log('Server listening on port 5000...');
+}) 
